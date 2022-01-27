f04ef95c615f7166d4e11ae54eac08c9a3e70976
joakimai <joakimai@kth.se>
2022-01-27 15:55:19 +0100

    test: Adds tests for LIC5.

--- src/test/java/DecideTest.java
+++ src/test/java/DecideTest.java
@@ -6,7 +6,7 @@ public class DecideTest {
 
     @Test
     public void givenDistanceGreaterThanLENGTH1BetweenTwoConsecutivePoints_whenLIC0_thenAssertTrue() {
-        double[][] points = new double[][]{{1.0D, 1.0D}, {100.0D, 100.0D}};
+        double[][] points = new double[][]{{1.0, 1.0}, {100.0, 100.0}};
         int numpoints = points.length;
         Parameters parameters = new Parameters(1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
         Decide decide = new Decide(numpoints, points, parameters, (LCM)null, (boolean[])null);
@@ -16,7 +16,7 @@ public class DecideTest {
 
     @Test
     public void givenDistanceGreaterThanLENGTH1DoesNotExistBetweenTwoConsecutivePoints_whenLIC0_thenAssertFalse() {
-        double[][] points = new double[][]{{1.0D, 1.0D}, {1.1, 1.1}};
+        double[][] points = new double[][]{{1.0, 1.0}, {1.1, 1.1}};
         int numpoints = points.length;
         Parameters parameters = new Parameters(1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
         Decide decide = new Decide(numpoints, points, parameters, (LCM)null, (boolean[])null);
@@ -26,7 +26,7 @@ public class DecideTest {
 
     @Test
     public void givenLENGTH1IsLessThanZero_whenLIC0_thenAssertFalse() {
-        double[][] points = new double[][]{{1.0D, 1.0D}, {100.0, 100.0}};
+        double[][] points = new double[][]{{1.0, 1.0}, {100.0, 100.0}};
         int numpoints = points.length;
         Parameters parameters = new Parameters(-1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
         Decide decide = new Decide(numpoints, points, parameters, (LCM)null, (boolean[])null);
