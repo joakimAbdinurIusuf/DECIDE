623f43329b024fbf07596c0512f5208ec8a3b4d9
joakimai <joakimai@kth.se>
2022-01-27 11:02:36 +0100

    refactor: Initializes data in unit tests instead of initializing data in the constructor.

--- src/test/java/DecideTest.java
+++ src/test/java/DecideTest.java
@@ -5,32 +5,26 @@ import static org.junit.jupiter.api.Assertions.*;
 public class DecideTest {
 
     private Parameters parameters;
-    private double[][] points;
-    private Decide decide;
-
-    private double[][] points2;
-    private Decide decide2;
 
     public DecideTest() {
         parameters = new Parameters();
-        points = new double[][]{{1.0D, 1.0D}, {100.0D, 100.0D}};
-        int numpoints = points.length;
-        decide = new Decide(numpoints, points, parameters, (LCM)null, (boolean[])null);
-
-        points2 = new double[][]{{1.0D, 1.0D}, {1.1, 1.1}};
-        int numpoints2 = points2.length;
-        decide2 = new Decide(numpoints2, points2, parameters, (LCM)null, (boolean[])null);
     }
 
     @Test
-    public void LIC0Pass() {
+    public void givenDistanceGreaterThanLENGTH1BetweenTwoConsecutivePoints_whenLIC0_thenAssertTrue() {
+        double[][] points = new double[][]{{1.0D, 1.0D}, {100.0D, 100.0D}};
+        int numpoints = points.length;
+        Decide decide = new Decide(numpoints, points, parameters, (LCM)null, (boolean[])null);
         boolean LIC0True = decide.LIC0();
         assertTrue(LIC0True);
     }
 
     @Test
-    public void LIC0Fail() {
-        boolean LIC0False = decide2.LIC0();
+    public void givenDistanceGreaterThanLENGTH1DoesNotExistBetweenTwoConsecutivePoints_whenLIC0_thenAssertFalse() {
+        double[][] points = new double[][]{{1.0D, 1.0D}, {1.1, 1.1}};
+        int numpoints = points.length;
+        Decide decide = new Decide(numpoints, points, parameters, (LCM)null, (boolean[])null);
+        boolean LIC0False = decide.LIC0();
         assertFalse(LIC0False);
     }
 }
