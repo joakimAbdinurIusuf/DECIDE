424ca01525a5e0bd164ff052ce3621715da94084
Anna Skantz <anna.skantz98@gmail.com>
2022-01-27 18:29:51 +0100

    bug: changes incorrect array indexing points[i+2][1] to points[i+1][1]

--- src/main/java/Decide.java
+++ src/main/java/Decide.java
@@ -58,7 +58,7 @@ public class Decide {
         for (int i = 0; i < numpoints-2; i++){
             firstTerm = points[i][0] * (points[i+1][1] - points[i+2][1]);
             secondTerm = points[i+1][0] * (points[i+2][1] - points[i][1]);
-            thirdTerm = points[i+2][0] * (points[i][1] - points[i+2][1]);
+            thirdTerm = points[i+2][0] * (points[i][1] - points[i+1][1]);
 
             triangleArea = Math.abs(firstTerm + secondTerm + thirdTerm)/2;
 
