cde2ebc1d8f50eea9b0de417be7f545834411178
joakimai <joakimai@kth.se>
2022-01-27 15:08:02 +0100

    refactor: Initializing the object using the constructor with more parameters.

--- src/test/java/DecideTest.java
+++ src/test/java/DecideTest.java
@@ -8,7 +8,7 @@ public class DecideTest {
     public void givenDistanceGreaterThanLENGTH1BetweenTwoConsecutivePoints_whenLIC0_thenAssertTrue() {
         double[][] points = new double[][]{{1.0D, 1.0D}, {100.0D, 100.0D}};
         int numpoints = points.length;
-        Parameters parameters = new Parameters(1);
+        Parameters parameters = new Parameters(1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
         Decide decide = new Decide(numpoints, points, parameters, (LCM)null, (boolean[])null);
         boolean LIC0True = decide.LIC0();
         assertTrue(LIC0True);
@@ -18,7 +18,7 @@ public class DecideTest {
     public void givenDistanceGreaterThanLENGTH1DoesNotExistBetweenTwoConsecutivePoints_whenLIC0_thenAssertFalse() {
         double[][] points = new double[][]{{1.0D, 1.0D}, {1.1, 1.1}};
         int numpoints = points.length;
-        Parameters parameters = new Parameters(1);
+        Parameters parameters = new Parameters(1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
         Decide decide = new Decide(numpoints, points, parameters, (LCM)null, (boolean[])null);
         boolean LIC0False = decide.LIC0();
         assertFalse(LIC0False);
