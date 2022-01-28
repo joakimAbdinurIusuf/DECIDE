110f908b4565d72bfd71e56e0d27b2eb6b391912
Anna Skantz <anna.skantz98@gmail.com>
2022-01-28 14:41:37 +0100

    test: adds tests for LIC6

--- src/test/java/DecideTest.java
+++ src/test/java/DecideTest.java
@@ -99,4 +99,45 @@ public class DecideTest {
         boolean LIC5False = decide.LIC5();
         assertFalse(LIC5False);
     }
+
+    @Test
+    public void givenDistanceBetweenPointAndLineGreaterThanDISTInSetOfNPTSConsecutivePoints_whenLIC6_thenAssertTrue() {
+        double[][] points = new double[][]{{0.0, 0.0}, {2.0, 2.0}, {4.0, 0.0}};
+        int numpoints = points.length;
+        Parameters parameters = new Parameters(0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+        Decide decide = new Decide(numpoints, points, parameters, (LCM)null, (boolean[])null);
+        boolean LIC6True = decide.LIC6();
+        assertTrue(LIC6True);
+    }
+
+    @Test
+    public void givenDoesNotExistDistanceBetweenPointAndLineGreaterThanDISTInSetOfNPTSConsecutivePoints_whenLIC6_thenAssertFalse() {
+        double[][] points = new double[][]{{0.0, 0.0}, {2.0, 2.0}, {4.0, 0.0}};
+        int numpoints = points.length;
+        Parameters parameters = new Parameters(0, 0, 0, 0, 0, 0, 10, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+        Decide decide = new Decide(numpoints, points, parameters, (LCM)null, (boolean[])null);
+        boolean LIC6False = decide.LIC6();
+        assertFalse(LIC6False);
+    }
+
+    @Test
+    public void givenDISTIsLessThanZero_whenLIC6_thenExceptionIsThrownAndMessagePrinted() {
+        double[][] points = new double[][]{{1.0, 1.0}, {100.0, 100.0}};
+
+        int numpoints = points.length;
+        Parameters parameters = new Parameters(0, 0, 0, 0, 0, 0, -1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+        Decide decide = new Decide(numpoints, points, parameters, (LCM)null, (boolean[])null);
+
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            decide.LIC6();
+        });
+
+        String expectedMessage = "DIST cannot be negative.";
+        String actualMessage = exception.getMessage();
+
+        assertTrue(actualMessage.contains(expectedMessage));
+    }
+
+
+
 }
