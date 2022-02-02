1068f3b9916cca68f8c36952414f9a4ee3e718a3
joakimai <joakimai@kth.se>
2022-02-02 09:36:56 +0100

    tests: Adds extra points and values for A_PTS and B_PTS.

--- src/test/java/DecideTest.java
+++ src/test/java/DecideTest.java
@@ -719,15 +719,16 @@ public class DecideTest {
     // LIC13
 
     /**
-     * Check that LIC13 returns true if there exists at least one set of three consecutive data points
+     * Check that LIC13 returns true if there exists at least one set of three consecutive data points,
+     * separated by exactly A_PTS and B PTS consecutive intervening points, respectively,
      * that cannot all be contained within or on a circle of radius RADIUS1, and another set that can
      * be contained within or on a circle of RADIUS2.
      */
     @Test
     public void LIC13PositiveCase() throws IllegalParameterObjectException {
-        double[][] points = new double[][]{{0.0, 0.0}, {0.0, 0.0}, {0.0, 0.0}, {-10.0, -10.0}, {10.0, 10.0}};
+        double[][] points = new double[][]{{0.0, 0.0}, {0.0, 0.0}, {0.0, 0.0}, {0.0, 0.0}, {0.0, 0.0}, {-10.0, -10.0}, {0.0, 0.0}, {0.0, 0.0}, {10.0, 10.0}};
         int numpoints = points.length;
-        Parameters parameters = new Parameters(0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 50, 0);
+        Parameters parameters = new Parameters(0, 1, 0, 0, 0, 0, 0, 0, 0, 2, 2, 0, 0, 0, 0, 0, 0, 50, 0);
         Decide decide = new Decide(numpoints, points, parameters, (LCM)null, (boolean[])null);
         boolean LIC13True = decide.LIC13();
         assertTrue(LIC13True);
@@ -735,13 +736,14 @@ public class DecideTest {
 
     /**
      * Check that LIC13 returns false if there does not exist at least one set of three consecutive data
-     * points that cannot all be contained within or on a circle of radius RADIUS1.
+     * points, separated by exactly A_PTS and B PTS consecutive intervening points, respectively,
+     * that cannot all be contained within or on a circle of radius RADIUS1.
      */
     @Test
     public void LIC13NegativeCaseRadius1() throws IllegalParameterObjectException {
-        double[][] points = new double[][]{{0.0, 0.0}, {0.0, 0.0}, {0.0, 0.0}, {-10.0, -10.0}, {10.0, 10.0}};
+        double[][] points = new double[][]{{0.0, 0.0}, {0.0, 0.0}, {0.0, 0.0}, {0.0, 0.0}, {0.0, 0.0}, {-10.0, -10.0}, {0.0, 0.0}, {0.0, 0.0}, {10.0, 10.0}};
         int numpoints = points.length;
-        Parameters parameters = new Parameters(0, 50, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 50, 0);
+        Parameters parameters = new Parameters(0, 50, 0, 0, 0, 0, 0, 0, 0, 2, 2, 0, 0, 0, 0, 0, 0, 50, 0);
         Decide decide = new Decide(numpoints, points, parameters, (LCM)null, (boolean[])null);
         boolean LIC13False = decide.LIC13();
         assertFalse(LIC13False);
@@ -749,13 +751,14 @@ public class DecideTest {
 
     /**
      * Check that LIC13 returns false if there does not exist at least one set of three consecutive data
-     * points that can all be contained within or on a circle of radius RADIUS2.
+     * points, separated by exactly A_PTS and B PTS consecutive intervening points, respectively,
+     * that can all be contained within or on a circle of radius RADIUS2.
      */
     @Test
     public void LIC13NegativeCaseRadius2() throws IllegalParameterObjectException {
-        double[][] points = new double[][]{{0.0, 0.0}, {0.0, 0.0}, {0.0, 0.0}, {-10.0, -10.0}, {10.0, 10.0}};
+        double[][] points = new double[][]{{0.0, 0.0}, {0.0, 0.0}, {0.0, 0.0}, {0.0, 0.0}, {0.0, 0.0}, {-10.0, -10.0}, {0.0, 0.0}, {0.0, 0.0}, {10.0, 10.0}};
         int numpoints = points.length;
-        Parameters parameters = new Parameters(0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0);
+        Parameters parameters = new Parameters(0, 1, 0, 0, 0, 0, 0, 0, 0, 2, 2, 0, 0, 0, 0, 0, 0, 1, 0);
         Decide decide = new Decide(numpoints, points, parameters, (LCM)null, (boolean[])null);
         boolean LIC13False = decide.LIC13();
         assertFalse(LIC13False);
@@ -763,15 +766,15 @@ public class DecideTest {
 
     /**
      * Check that LIC13 returns false if there does not exist at least one set of three consecutive data
-     * points that cannot all be contained within or on a circle of radius RADIUS1, and if there does not
+     * points, separated by exactly A_PTS and B PTS consecutive intervening points, respectively,
+     * that cannot all be contained within or on a circle of radius RADIUS1, and if there does not
      * exist another set that can be contained within or on a circle of RADIUS2.
-     * .
      */
     @Test
     public void LIC13NegativeCaseBothConditionsFalse() throws IllegalParameterObjectException {
-        double[][] points = new double[][]{{0.0, 0.0}, {0.0, 0.0}, {0.0, 0.0}, {-10.0, -10.0}, {10.0, 10.0}};
+        double[][] points = new double[][]{{0.0, 0.0}, {0.0, 0.0}, {0.0, 0.0}, {0.0, 0.0}, {0.0, 0.0}, {-10.0, -10.0}, {0.0, 0.0}, {0.0, 0.0}, {10.0, 10.0}};
         int numpoints = points.length;
-        Parameters parameters = new Parameters(0, 50, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0);
+        Parameters parameters = new Parameters(0, 50, 0, 0, 0, 0, 0, 0, 0, 2, 2, 0, 0, 0, 0, 0, 0, 1, 0);
         Decide decide = new Decide(numpoints, points, parameters, (LCM)null, (boolean[])null);
         boolean LIC13False = decide.LIC13();
         assertFalse(LIC13False);
@@ -779,7 +782,6 @@ public class DecideTest {
 
     /**
      * Check that LIC13 returns false when numpoints < 5.
-     * .
      */
     @Test
     public void LIC13NegativeCaseNumpoints() throws IllegalParameterObjectException {
